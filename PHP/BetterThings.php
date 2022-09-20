097d84228b619120825dd766beb7526d451b57e1
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-09-20 10:39:51 +0200

    Update BetterThings.php

--- PHP/BetterThings.php
+++ PHP/BetterThings.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html><!--copyright Wither362, 2021-2022, DO NOT COPY!! unless you credit me pinche hijo pvta -->
 <html>
 <head>
 </head>
@@ -6,12 +6,13 @@
 <?php
 $noes = 'no especificado.<br>';
 $frutas = array( // nombre - colores<Array> - se usa en<Array>
-  array('Manzana', array('Amarillo', 'Verde', 'Rojo'), array('Tartas', 'Pasteles', 'Compota', 'Zumo')),
+  array('Manzana', array('Amarillo', 'Verde', 'Rojo'), array('Tartas', 'Pasteles', 'Compota', 'Zumo'), array('Manzano')),
   array('Plátano', array('Amarillo', 'Marrón'), array('Tartas', 'Pasteles', 'Batido')),
   array('Limón', array('Amarillo'), array('Tartas', 'Pasteles', 'Limonada')),
   array('Naranja', array('Naranja'), array('Tartas', 'Pasteles', 'Zumos')),
   array('Albaricoque', array('Naranja', 'Marrón')),
   array('Fresa', array('Roja', 'Marrón'), array('Tartas', 'Pasteles', 'Batido')),
+  array('Fresón', array('Rojo', 'Marrón', 'Blanco')),
   array('Arándano', array('Azul'))
 );
 $comidas = array( // nombre - ingredientes
@@ -51,6 +52,24 @@ for ($i = 0; $i < count($frutas); $i++) {
       if($frutas[$i][2][$a + 2] == null && $frutas[$i][2][$a + 1] != null) {
         echo ' y ';
       } else if($frutas[$i][2][$a + 1] != null) {
+        echo ', ';
+      } else {
+        echo '.<br>';
+      }
+    }
+  } else {
+    echo $noes;
+  }
+}
+echo '<br><h3>Árbol</h3>';
+for ($i = 0; $i < count($frutas); $i++) {
+  echo $frutas[$i][0], ': ';
+  if($frutas[$i][3] != null) {
+    for ($a = 0; $a < count($frutas[$i][3]); $a++) {
+      echo $frutas[$i][3][$a];
+      if($frutas[$i][3][$a + 2] == null && $frutas[$i][3][$a + 1] != null) {
+        echo ' y ';
+      } else if($frutas[$i][3][$a + 1] != null) {
         echo ', ';
       } else {
         echo '.<br>';
